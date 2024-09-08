22be3e9b65b6122a7f2405f0805453b6cebae958
A.Ridyard <34243370+Ridyard@users.noreply.github.com>
2024-09-08 17:54:13 +0100

    Bug fixes and logic amendments
    bug fix:
    if a track t had multiple contributors, any additional artist would be assigned to the t+1 track, meaning results weren't entirely accurate
    
    bug fix:
    showAllClick() funuction wasn't showing all tracks. I set the showAllClick() target xpath within the scope of the parent_div and this resolved the issue

--- Soundtrack Scraper.py
+++ Soundtrack Scraper.py
@@ -27,15 +27,17 @@ def findGivenElements(xpath_in):
     return browser.find_elements(By.XPATH, xpath_in)
 
 
-def showAllClick():
+def showAllClick(parent_div_in):
     # each episode-page will show an abridged list of tracks; if there is a "show all" button, this function will click & expand the track listing
     try:
         showAllButt_xpath = '//p[@class="sc-jEACwC jpIqjk sc-hmdomO YqQqi"]'
-        showAllButt = WebDriverWait(browser, 10).until(
-            EC.element_to_be_clickable((By.XPATH, showAllButt_xpath)))
+        showAllButt = parent_div_in.find_element(By.XPATH, showAllButt_xpath)
+        browser.execute_script("arguments[0].scrollIntoView(true);", showAllButt)
         browser.execute_script("arguments[0].click();", showAllButt)
-    except:
-        print("No 'Show All' button found, moving on.")
+        print("'show all' button clicked")
+    except Exception as e:
+        print(f"No 'Show All' button found or issue with clicking: {str(e)}")
+
 
 
 playlist = {}  # dict to hold each artist:song kv pair
@@ -81,24 +83,26 @@ for j in range(len(episode_elements)):
     try:
         browser.execute_script("arguments[0].click();", episode)  # click into each episode
         
-        # once inside the episode page, scrape the tracks
-        showAllClick()  # Click "Show All" if present
-                
         # pull song / artist elements from within a specific div element
         # avoids pulling additional / not required tracks that are duplicated around the page
         div_xpath = '//div[@class="sc-hBtRBD iUapDl"]'
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, div_xpath))) # Wait for the div to be present in the DOM
-
+        
         # Locate the parent div element
         parent_div = browser.find_element(By.XPATH, div_xpath)
 
+        # the "show all tracks" button is located within the parent_div, hence why we pass the parent_div as an argument
+        # this function call is inconsistent when we don't use the parent_div, ie we just search for the "show all tracks" button xpath
+        showAllClick(parent_div)  # Click "Show All" if present
+
+        # once inside the episode page, scrape the tracks
         # Now find a specific song(p) & artist(a/small) elements within the parent div
         song_elements = parent_div.find_elements(By.XPATH, './/p[@class="sc-jEACwC jpIqjk sc-hmdomO YqQqi sc-knuQbY dhEDwX"]')
-        artist_elements = parent_div.find_elements(By.XPATH, './/a/small')
-        
+        artist_elements = parent_div.find_elements(By.XPATH, ".//div[contains(@class, 'ant-row') and contains(@class, 'sc-ERObt') and contains(@class, 'fRngQV')]") # this will pull the full artist list for each track, ie if there are multiple contributors
+
         # scrape song & artist into separate lists
         song_text = [song.text for song in song_elements]
-        artist_text = [artist.text for artist in artist_elements]
+        artist_text = [artist.text.replace('\n', '') for artist in artist_elements] # if there are multiple artists, this line will remove the /n separator 
 
         playlist.update(dict(zip(song_text, artist_text))) # combine the song with the corresponding artist
 
